34331c5728ca4edc0acbb8a156c9818db605684c
Yao <gyy990827@outlook.com>
2021-06-25 16:55:41 +0800

    0.1

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,37 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <img src="./assets/logo.png" />
     <div>
       <el-button @click="startHacking">Start</el-button>
+    </div>
+    <div id="app">
+      <input v-model.lazy="test" />
+      <!-- <input :value="test" @input="test= $event.target.value"> --><!--语法糖-->
+      <input :value="test" @input="test = $event.target.value" />
     </div>
   </div>
 </template>
 
+
 <script>
 export default {
+  data() {
+    return {
+      test: "这是一个测试",
+    };
+  },
   methods: {
-    startHacking () {
+    startHacking() {
       this.$notify({
-        title: 'It works!',
-        type: 'success',
-        message: 'We\'ve laid the ground work for you. It\'s time for you to build something epic!',
-        duration: 5000
-      })
-    }
-  }
-}
+        title: "It works!",
+        type: "success",
+        message:
+          "We've laid the ground work for you. It's time for you to build something epic!",
+        duration: 5000,
+      });
+    },
+  },
+};
 </script>
 
 <style>
